faae4af81f3ac991d7c27d5fb031be596d9652db
KaleyMoro <csrijan10@gmail.com>
2019-09-12 23:51:49 -0700

    initial commit

--- nekoxyz.js
+++ nekoxyz.js
@@ -5,20 +5,20 @@ function getContent(url) {
   return new Promise((resolve, reject) => {
 
     try {
-        fetch(url)
+      fetch(url)
         .then(function (response) {
           return response.json();
         })
         .then(function (jsonObj) {
-            resolve(jsonObj)
+          resolve(jsonObj)
           //console.log(jsonObj);
-    
+
         })
     } catch (error) {
-        reject(`Error: ${error.message}`)
+      reject(`Error: ${error.message}`)
     }
 
-    
+
   });
 }
 class NekoClient {
@@ -32,15 +32,15 @@ class NekoClient {
       this[endpoint] = async function () { return await getContent(baseURL + endpoints.data[endpoint]); };
     });
     Object.keys(endpoints.imagegen).forEach(async (endpoint) => {
-      this.imagegen[endpoint] = async function (queryParams ,queryParams2) {
-          let url = new URL(`${baseURL}${endpoints.imagegen[endpoint]}&user1=${queryParams}&user2=${queryParams2}`);
+      this.imagegen[endpoint] = async function (queryParams, queryParams2) {
+        let url = new URL(`${baseURL}${endpoints.imagegen[endpoint]}&user1=${queryParams}&user2=${queryParams2}`);
         queryParams == '' ? url.search = new URLSearchParams(queryParams) : '';
-        queryParams2 =='' ? url.search = new URLSearchParams(queryParams2) : '';
+        queryParams2 == '' ? url.search = new URLSearchParams(queryParams2) : '';
         //console.log(url)
         return await getContent(url.toString());
-        };
+      };
     });
-   }
+  }
 }
 
 module.exports = NekoClient;
